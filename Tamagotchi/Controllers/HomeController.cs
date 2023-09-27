d2892e22b99d67eef2474d0b037be6bb458c5c52
Richard Barbour <richyb07@gmail.com>
2023-09-27 10:46:23 -0700

    Complete Views/Home/Index.cshtml and HomeController.cs.
    Co-authored-by: Joey Palchak <joey.palchak@gmail.com>
    Co-authored-by: Jon Cheng <joncheng.dev@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>

--- Tamagotchi/Controllers/HomeController.cs
+++ Tamagotchi/Controllers/HomeController.cs
@@ -1,1 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
+
+namespace Tamagotchi.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View(); 
+    }
+  }
+}
